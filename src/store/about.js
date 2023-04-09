b0e12dce7fcfb3f1c388a5704fd907b1b6e3e143
yaribo <youngrykim@kakao.com>
2023-04-10 08:25:03 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'Yaribo',
+    email: 'thesecon@gmail.com',
+    blog: 'https://heropy.bolg',
+    phone: '+82-01-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
